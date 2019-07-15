5529b13815ed21e2211d7bd69eb3964fdf6ac503
TheHaunted <kevin_lopez@live.com>
2019-07-15 16:09:03 -0500

    MVP: added click listener to buttons to update scores

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [scoreLions, setScoreLions] = useState(0);
-  const [scoreTigers, setScoreTigers] = useState(0);
+  let [scoreLions, setScoreLions] = useState(0);
+  let [scoreTigers, setScoreTigers] = useState(0);
 
   return (
     <div className="container">
@@ -31,12 +31,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={ () => setScoreLions(scoreLions + 7) }>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={ () => setScoreLions(scoreLions + 1) }>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={ () => setScoreTigers(scoreTigers + 7) }>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={ () => setScoreTigers(scoreTigers + 1) }>Away Field Goal</button>
         </div>
       </section>
     </div>
